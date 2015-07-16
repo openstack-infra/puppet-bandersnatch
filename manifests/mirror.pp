d6a4cfd7a5dddd2018cce39283e452e711c65af6
Nikki Heald <nicky@notnowlewis.com>
2015-07-16 09:57:53 +0100

    Run cronjob in a login shell with fixed logging
    Specifies the cronjob should run in an sh login shell
    so that all environment variables are picked up. This
    is required for systems that go through a http proxy.
    
    Also, uses logger for logging output instead of redirecting
    to a log file.
    
    Change-Id: I8f15260f57361ab2df5ddc5d81338905be86e0e7

--- manifests/mirror.pp
+++ manifests/mirror.pp
@@ -67,7 +67,7 @@ class bandersnatch::mirror (
 
   cron { 'bandersnatch':
     minute      => '*/5',
-    command     => 'flock -n /var/run/bandersnatch/mirror.lock timeout -k 2m 30m run-bandersnatch >>/var/log/bandersnatch/mirror.log 2>&1',
+    command     => '/bin/sh -l -c "flock -n /var/run/bandersnatch/mirror.lock timeout -k 2m 30m run-bandersnatch | logger -t bandersnatch"',
     environment => 'PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin',
   }
 }
