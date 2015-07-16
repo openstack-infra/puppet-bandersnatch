4cd02924f613420a0b15c777df1e70127dd66b02
Nicola Heald <nicola.heald@hp.com>
2015-07-16 14:21:54 +0100

    Simple index daily snapshotting
    Optional snapshotting of the simple index,
    every day on first sync. Configurable number of
    days retention. Snapshotting off by default.
    
    When snapshotting, we should keep all packages
    
    To prevent older snapshots from becoming invalid, we need
    to keep all packages, even if they're gone from p.p.o
    
    To keep disk usage sane, we'll need a further change that
    runs a script to remove packages that are no longer referenced
    from any of the snapshots. That will come in a future review.
    
    Rebased-By: Ramy.Asselin@hpe.com
    Change-Id: I480b7839d39434d1ac6ba7bccce124d16e750884

---
+++ files/run_snapshotting.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# simple snapshotting mechanism
+
+MIRROR_PATH=$1
+DAYS_RETENTION=$2
+SNAPSHOT_NAME=`date +snapshot-%Y-%m-%d`
+
+cd ${MIRROR_PATH}
+
+# taking a daily snapshot, if it already exists, exit
+# this allows multiple mirror updates per day, but
+# only one snapshot
+if [ -d "${SNAPSHOT_NAME}" ]; then
+    exit 0
+fi
+
+# packages are in a central place, so just clone the index
+echo "Creating simple index snapshot ${SNAPSHOT_NAME}"
+cp -r simple ${SNAPSHOT_NAME}
+
+# remove everything but the last $DAYS_RETENTION snapshots
+rm -rf `ls -1d snapshot*| head -n -${DAYS_RETENTION}`
+
+# snapshots are now available at http://your-mirror.network/snapshot-DATE/
