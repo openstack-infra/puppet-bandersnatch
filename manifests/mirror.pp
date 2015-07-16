6fbd8905e66597fe4869922be4120092e5786673
Nicola Heald <nicola.heald@hp.com>
2015-07-16 14:21:54 +0100

    Simple index daily snapshotting
    Optional snapshotting of the simple index,
    every day on first sync. Configurable number of
    days retention. Snapshotting off by default.
    
    When snapshotting, we should keep all packages
    
    To prevent older snapshots from becoming invalid, we need
    to keep all packages, even if they're gone from p.p.o
    
    To keep disk usage sane, we'll need a further change that
    runs a script to remove packages that are no longer referenced
    from any of the snapshots. That will come in a future review.
    
    Rebased-By: Ramy.Asselin@hpe.com
    Change-Id: I480b7839d39434d1ac6ba7bccce124d16e750884

--- manifests/mirror.pp
+++ manifests/mirror.pp
@@ -17,11 +17,13 @@
 # Class to set up bandersnatch mirroring.
 #
 class bandersnatch::mirror (
-  $group       = 'root',
-  $hash_index  = false,
-  $mirror_root = '/srv/static/mirror',
-  $static_root = '/srv/static',
-  $user        = 'root',
+  $daily_snapshots    = false,
+  $group              = 'root',
+  $hash_index         = false,
+  $mirror_root        = '/srv/static/mirror',
+  $snapshot_retention = 5,
+  $static_root        = '/srv/static',
+  $user               = 'root',
 ) {
 
   if ! defined(File[$static_root]) {
@@ -47,5 +49,27 @@ class bandersnatch::mirror (
   file { '/etc/bandersnatch.conf':
     ensure  => present,
     content => template('bandersnatch/bandersnatch.conf.erb'),
+  }
+
+  if $daily_snapshots {
+    $ensure_snapshot = 'present'
+  } else {
+    $ensure_snapshot = 'absent'
+  }
+
+  file { '/usr/local/bin/run-bandersnatch-snapshotting':
+    ensure => $ensure_snapshot,
+    source => 'puppet:///modules/bandersnatch/run_snapshotting.sh',
+    mode   => '0755',
+  }
+
+  cron { 'bandersnatch-snapshot':
+    ensure      => $ensure_snapshot,
+    user        => $user,
+    minute      => '10',
+    hour        => '*/1',
+    command     => "flock -w 300 /var/run/bandersnatch/mirror.lock run-bandersnatch-snapshotting ${mirror_root}/web ${snapshot_retention} >>/var/log/bandersnatch/mirror.log 2>&1",
+    environment => 'PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin',
+    require     => File['/usr/local/bin/run-bandersnatch-snapshotting'],
   }
 }
