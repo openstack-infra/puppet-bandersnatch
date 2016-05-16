3c86dd074bb62279eb918f1bafd9dc8246420cbe
Ramy Asselin <ramy.asselin@hpe.com>
2016-05-16 14:27:07 -0700

    Fix bandersnatch crons to support full sync
    Don't set a time limit on run-bandersnatch. Let it take as long as it needs,
    in case it needs to do a full sync since killing the process and restarting it
    may cause it to restart if the todo file isn't updated.
    
    Allow the user to optionally set a configurable timeout (with kill option)
    on the bandersnatch command.
    
    Log bandersnatch output as soon as it is available instead of after
    the (full) sync completes. This is especially useful and necessary
    to track progress of full resyncs which take a several hours.
    
    Change-Id: Ia3ff98ae22641a72b97151804076384b97033a6a

--- manifests/cron.pp
+++ manifests/cron.pp
@@ -16,14 +16,39 @@
 #
 # Class to set up bandersnatch cron job.
 #
+# === Parameters
+#
+# [*timeout_duration*]
+#  The bandersnatch command has been know to hang, causing the local mirror
+#  to get out of sync. After an initial full-sync is complete (which takes
+#  several hours), subsequent delta invocations are usually complete in
+#  minutes. Set this option to e.g. '4h' duration to limit the amount of time
+#  the process is possibly hanged, while still allowing longer deltas to sync.
+#  You should NOT use this setting until after a full sync has been completed.
+#  Otherwise, if the process aborts before the full sync completed, the full
+#  sync will start over from the beginning and may never complete.
+#  See 'man timeout'
+#
+# [*timeout_kill_duration*]
+#   Optionally, force the bandersnatch process to be killed if it is still
+#   running e.g. '2m' after the timeout period. See 'man timeout' option -k.
+#
+# [*user*]
+#   The user to run the bandersnatch cron job
+#
+
 class bandersnatch::cron (
+  $timeout_duration = undef,
+  $timeout_kill_duration = undef,
   $user = 'root',
 ) {
+
+  $cron_timeout = inline_template("<% if @timeout_duration -%> timeout <% if @timeout_kill_duration -%> -k ${timeout_kill_duration} <% end -%> ${timeout_duration} <% end -%>")
 
   cron { 'bandersnatch':
     user        => $user,
     minute      => '*/5',
-    command     => 'flock -n /var/run/bandersnatch/mirror.lock timeout -k 2m 30m run-bandersnatch >>/var/log/bandersnatch/mirror.log 2>&1',
+    command     => "flock -n /var/run/bandersnatch/mirror.lock ${cron_timeout} run-bandersnatch >>/var/log/bandersnatch/mirror.log 2>&1",
     environment => 'PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin',
   }
 }
