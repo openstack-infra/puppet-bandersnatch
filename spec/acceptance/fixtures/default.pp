e264752002b5699a38083e60d0e19000c8eb2584
Colleen Murphy <colleen@gazlene.net>
2016-05-23 10:12:31 -0700

    Fix acceptance tests
    In 2940279 management of the apache vhost was split into a separate
    class and the vhost_name parameter was removed from the
    bandersnatch::mirror class. This wasn't done in a backwards compatible
    way and the acceptance tests were never updated to use the new API.
    This patch updates the tests to use the new class and to stop using the
    now invalid parameter.
    
    Change-Id: If724c51b72f634a95bbffd080a9d33234a5d7645

--- spec/acceptance/fixtures/default.pp
+++ spec/acceptance/fixtures/default.pp
@@ -1,7 +1,10 @@
 class { '::bandersnatch':
 }
 
-class { '::bandersnatch::mirror':
+class { '::bandersnatch::httpd':
   vhost_name => '127.0.0.1',
+}
+
+class { '::bandersnatch::mirror':
   require    => Class['::bandersnatch'],
 }
